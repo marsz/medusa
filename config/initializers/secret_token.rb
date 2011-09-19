9deaf7b0690c3202edbdb6c6ba8c673aa3176099
marsz <marsz330@gmail.com>
2011-09-19 18:14:13 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medusa::Application.config.secret_token = 'afa30c9207d282b0ccd6b3343c08c17fd41177391cf49fdd1230a4d0f39f3f0d1c136247611d00d62957caa2d0dc729237652cb2ac45cd233f0059ae8f74fc75'
