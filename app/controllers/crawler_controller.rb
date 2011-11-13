79a677e7fb1f6d10c08602b364c4905e48297669
marsz <marsz330@gmail.com>
2011-11-13 23:07:32 +0800

    authenticate & log

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -1,10 +1,16 @@
 class CrawlerController < ApplicationController
+  include ActAsAuthable
+  act_as_authable
+  
   def fetch
     if params[:url]
       params[:options] ||= {}
       params[:options][:method] = request.method.downcase      
       result = {:method => params[:options][:method],:url => params[:url]}
       result[:data] = DomainCrawling.crawling(params[:url], params[:query], params[:options])
+      if @app && !params[:options][:disable_log]
+        log_crawling(@app, params[:url])
+      end
       respond_to do |f|
         f.html {render :text => result[:data].to_s}
         f.json {render :json => result.to_json}
---
+++ lib/act_as_authable.rb
@@ -0,0 +1,27 @@
+module ActAsAuthable
+  extend ActiveSupport::Concern
+  module ClassMethods
+    def act_as_authable
+      before_filter :authenticate_app
+    end
+  end
+  module InstanceMethods
+    def authenticate_app
+      if @app = authenticating(params[:token])
+      else
+        render :status => 500, :text => "token fail"
+      end
+    end
+    protected
+    def log_crawling app, url
+      AppCrawling.create!(:app => app, :url => url)
+    end
+    def authenticating token
+      app = nil
+      if !token.blank?
+        app = App.find_by_token(params[:token])
+      end
+      app
+    end
+  end
+end
