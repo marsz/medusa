c6778c24508d9afc2caf855627d6fb010e614eaa
marsz <marsz330@gmail.com>
2011-12-27 18:45:57 +0800

    modify for forked project setup

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,10 @@
 Airbrake.configure do |config|
   if ["production","development"].include?(Rails.env)
-    config.api_key = YAML.load(File.open('config/airbrake.yml'))[Rails.env][:api_key]
+    begin
+      config.api_key = YAML.load(File.open('config/airbrake.yml'))[Rails.env][:api_key]
+    rescue Errno::ENOENT
+      puts "warring: you don't have set config/airbrake.yml"
+    end
   end
   # config.development_environments = ["test"]
 end
