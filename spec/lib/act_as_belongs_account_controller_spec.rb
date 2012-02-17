140100f6dd6a75ae4a11eb96845fc033e086647f
marsz <marsz330@gmail.com>
2012-02-18 02:00:38 +0800

    pending lib/act_as_belongs_to_account

--- spec/lib/act_as_belongs_account_controller_spec.rb
+++ spec/lib/act_as_belongs_account_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
-describe "act_as_belongs_account_controller" do
-  pending 
+describe ActAsBelongsAccountController, :type => :controller do
+  pending
+  # controller(SpidersController) do
+  #     include ActAsBelongsAccountController
+  #     def show
+  #       render :text => "xx"
+  #     end
+  # end
+  # 
+  # describe "qqq" do
+  #   it "aaa" do
+  #     account = Factory :account
+  #     get :show, :account_id => account.id
+  #     response.status.should be_success
+  #     assigns[:account].should be_a_kind_of(Account)
+  #   end
+  # end
+  
 end
